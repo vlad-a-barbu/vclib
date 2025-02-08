5167e5d3d8eeaa93ce7a9093da2d410d3d953c16
Vlad A. Barbu <vlad.a.brb@gmail.com>
2025-02-08 12:50:16 +0200

    fix mem leaks

--- protohacks/echo/main.c
+++ protohacks/echo/main.c
@@ -7,6 +7,16 @@
 #include <errno.h>
 #include <assert.h>
 
+#define PORT "4200"
+
+#ifndef DEBUG
+#define DBG 0
+#define NDBG 1
+#else
+#define DBG 1
+#define NDBG 0
+#endif
+
 #define ALLOC(sz) malloc(sz)
 #define REALLOC(ptr, sz) realloc(ptr, sz)
 #define FREE(ptr) free(ptr)
@@ -19,15 +29,16 @@ struct vb_darray
 };
 
 struct vb_darray
-create_darray(size_t cap)
+vb_create_darray(size_t cap)
 {
-    char *ptr = ALLOC(cap);
+    assert(cap > 0 && "invalid initial cap");
+    char *ptr = (char *)ALLOC(cap);
     assert(ptr && "allocation failed");
     struct vb_darray res = {ptr, 0, cap};
     return res;
 }
 
-void destroy_darray(struct vb_darray *darray)
+void vb_destroy_darray(struct vb_darray *darray)
 {
     assert(darray && darray->ptr && "null ptrs");
     FREE(darray->ptr);
@@ -35,30 +46,26 @@ void destroy_darray(struct vb_darray *darray)
     darray->cap = 0;
 }
 
-void darray_push(struct vb_darray *darray, const char *ptr, size_t len)
+void vb_darray_push(struct vb_darray *darray, const char *ptr, size_t len)
 {
     assert(darray && darray->ptr && "nulls ptrs");
 
     if (darray->len + len > darray->cap)
     {
-        size_t new_cap;
-        if (len > darray->cap)
-            new_cap = len * 2;
-        else
-            new_cap = darray->cap * 2;
-
-        darray->ptr = REALLOC(darray->ptr, new_cap);
-        darray->cap = new_cap;
+        while (darray->len + len > darray->cap)
+            darray->cap *= 2;
+        darray->ptr = (char *)REALLOC(darray->ptr, darray->cap);
     }
 
     char *dst = darray->ptr + darray->len;
     for (size_t i = 0; i < len; ++i)
-        *(dst + i) = *(ptr + i);
+        *dst++ = *ptr++;
 
     darray->len += len;
 }
 
-int main()
+// todo: err handling
+int listen_tcp()
 {
     int sfd = socket(PF_INET, SOCK_STREAM, 0);
     char reuse_addr = 1;
@@ -68,22 +75,31 @@ int main()
     hints.ai_family = PF_INET;
     hints.ai_socktype = SOCK_STREAM;
     struct addrinfo *addr;
-    getaddrinfo("127.0.0.1", "4200", &hints, &addr);
+    getaddrinfo("127.0.0.1", PORT, &hints, &addr);
     if (bind(sfd, addr->ai_addr, addr->ai_addrlen) != 0)
         perror("bind");
+    freeaddrinfo(addr);
     listen(sfd, 10);
-    printf("listening on port 4200\n");
+    printf("listening on port %s\n", PORT);
+}
 
-    while (1)
+int main()
+{
+    int sfd = listen_tcp();
+
+    do
     {
         struct sockaddr caddr;
         socklen_t caddr_len;
         int cfd = accept(sfd, &caddr, &caddr_len);
 
-        // recv
         char buff[0x100];
-        struct vb_darray darray = create_darray(0x100);
+        struct vb_darray darray = vb_create_darray(0x100);
+        char *send_ptr;
+        size_t send_len;
         char err = 0;
+
+        // recv
         while (1)
         {
             ssize_t n = recv(cfd, buff, sizeof(buff), 0);
@@ -95,7 +111,7 @@ int main()
                 }
                 break;
             }
-            darray_push(&darray, buff, n);
+            vb_darray_push(&darray, buff, n);
         }
         if (err)
         {
@@ -104,8 +120,8 @@ int main()
         }
 
         // send
-        char *send_ptr = darray.ptr;
-        size_t send_len = darray.len;
+        send_ptr = darray.ptr;
+        send_len = darray.len;
         while (1)
         {
             ssize_t n = send(cfd, send_ptr, send_len, 0);
@@ -117,6 +133,7 @@ int main()
                 }
                 break;
             }
+            send_ptr += n;
             send_len -= n;
         }
         if (err == 1)
@@ -127,9 +144,10 @@ int main()
         assert(send_len == 0 && "sent != recvd");
 
     end:
-        destroy_darray(&darray);
+        vb_destroy_darray(&darray);
         close(cfd);
-    }
+
+    } while (NDBG);
 
     return 0;
 }
